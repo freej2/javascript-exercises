495746a7ee1c6539210ea187e5c5c6a4a844af60
freej2 <40305268+freej2@users.noreply.github.com>
2024-11-06 20:08:21 -0700

    exercise 4 solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(array_original, ...items_to_remove) {
+    return array_original.filter(item => !items_to_remove.includes(item));
 };
 
 // Do not edit below this line
