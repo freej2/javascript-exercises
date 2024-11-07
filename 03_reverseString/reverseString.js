a9e452b04a3e31cf772d325874a83fe005d172ca
freej2 <40305268+freej2@users.noreply.github.com>
2024-11-06 19:57:09 -0700

    third exercise solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    const string_chars = stringToReverse.split('');
+    const reversed = string_chars.reverse();
+    reversed_string = reversed.join('');
+    return reversed_string;
 };
 
 // Do not edit below this line
