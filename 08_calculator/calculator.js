663ce265eefc5eff9737ae76c4c9d361e42eaba6
Jacob <40305268+freej2@users.noreply.github.com>
2024-11-10 11:23:19 -0800

    exercise 08 initial functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return (num1 + num2);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return (num1-num2);
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(num1, num2) {
+  return (num1*num2);
 };
 
-const power = function() {
-	
+const power = function(num1, power) {
+	return (num1 ** power)
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+  result = 1;
+	for (let number = num1; number > 0, number--;){
+    result *= number;
+  }
+  return result;
 };
 
 // Do not edit below this line
