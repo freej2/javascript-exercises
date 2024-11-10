5ff39244af37cdc1a711d84a515bd12ea0b31250
Jacob <40305268+freej2@users.noreply.github.com>
2024-11-10 11:28:51 -0800

    exercise 09 initial solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (stringFWD) {
+    // Remove all punctuation, spaces, and convert to lowercase
+    stringFWD = stringFWD.toLowerCase().replace(/[^a-z0-9]/g, '');
+    // Convert to array, reverse it, and join back to string
+    const stringBKWD = stringFWD.split('').reverse().join('');
+    return stringBKWD === stringFWD;
 };
 
 // Do not edit below this line
