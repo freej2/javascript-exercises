4fbdae45f73b3939799b4662f7b9798eb5ea4ea1
Jacob <40305268+freej2@users.noreply.github.com>
2024-11-10 11:39:26 -0800

    exercise 10 initial solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(FibIndex) {
+    if (FibIndex < 0){
+        return 'OOPS';
+    }
+    const FibNums = [0, 1];
 
+    for (let i = 2; i <= FibIndex; i++){
+        FibNums[i] = FibNums[i-2] + FibNums[i-1];
+    }
+    return FibNums[index];
 };
 
 // Do not edit below this line
