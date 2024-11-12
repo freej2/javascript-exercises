14ce5933c5082f2abe51e4eeb4087f3dc7090855
freej2 <40305268+freej2@users.noreply.github.com>
2024-11-11 22:36:09 -0700

    exercise 08 solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -10,8 +10,8 @@ const sum = function(nums) {
 	return nums.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function(num1, num2) {
-  return (num1*num2);
+const multiply = function(nums) {
+  return nums.reduce((product, current) => product * current, 1);
 };
 
 const power = function(num1, power) {
@@ -19,8 +19,8 @@ const power = function(num1, power) {
 };
 
 const factorial = function(num1) {
-  result = 1;
-	for (let number = num1; number > 0, number--;){
+  let result = 1;
+	for (let number = num1; number > 1; number--){
     result *= number;
   }
   return result;
